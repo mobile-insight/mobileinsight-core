5de8838a93ba03784e397d249e57a0a40ddceae7
likayo <likayo25@gmail.com>
2014-11-24 17:52:05 -0800

    Complete hdlc_parser.
    And fix a bug caused by escaped FCS.

--- hdlc_parser.py
+++ hdlc_parser.py
@@ -21,7 +21,7 @@ class hdlc_parser:
 
     def feed_binary(self, ts, binary):
         b_lst = binary.split("\x7e")
-        print repr(b_lst)
+        # print repr(b_lst)
         for i in range(len(b_lst)):
             b = b_lst[i]
             if i == len(b_lst) - 1:
@@ -51,10 +51,10 @@ class hdlc_parser:
     def _clean_escape(self, frame_binary):
         payld = []
         esc = False
-        for c in frame_binary[0:-2]:
+        for c in frame_binary:
             if esc:
                 orig_c = chr(ord(c) ^ 0x20)
-                print '7d %s ==> %s' % (binascii.b2a_hex(c), binascii.b2a_hex(orig_c))
+                # print '7d %s ==> %s' % (binascii.b2a_hex(c), binascii.b2a_hex(orig_c))
                 payld.append(orig_c)
                 esc = False
             else:
@@ -63,7 +63,8 @@ class hdlc_parser:
                 else:
                     payld.append(c)
         payld = ''.join(payld)
-        fcs = struct.unpack("<H", frame_binary[-2:])[0]   # unsigned short
+        fcs = struct.unpack("<H", payld[-2:])[0]   # unsigned short
+        payld = payld[0:-2]
         return payld, fcs
 
     def _check_crc(self, payld, fcs):
@@ -71,7 +72,7 @@ class hdlc_parser:
         for c in payld:
             crc16.update(c)
         calc_crc = struct.unpack(">H", crc16.digest())[0]
-        print 'Calc %s, FCS %s' % (hex(calc_crc), hex(fcs))
+        # print 'Calc %s, FCS %s' % (hex(calc_crc), hex(fcs))
         return calc_crc == fcs
 
     def __iter__(self):
@@ -86,13 +87,56 @@ class hdlc_parser:
 
 
 if __name__ == '__main__':
-    b = "7d 5d 02 88 13 a5 13 c3 40 7e".replace(" ", "")
-    b += "7d 5d 02 7c 15 8c 15 c0 02 7e".replace(" ", "")
-    b = binascii.a2b_hex(b)
+    import sys
+    # b = "7d 5d 02 88 13 a5 13 c3 40 7e".replace(" ", "")
+    # b += "7d 5d 02 7c 15 8c 15 c0 02 7e".replace(" ", "")
+    # b = binascii.a2b_hex(b)
+    # parser = hdlc_parser()
+    # parser.feed_binary(0.0, b[0:5])
+    # parser.feed_binary(0.5, b[5:7])
+    # parser.feed_binary(1.0, b[7:])
+    # for t, fr, fcs, crc_correct in parser:
+    #     print t, binascii.b2a_hex(fr), hex(fcs), crc_correct
+    # sys.exit(0)
 
-    parser = hdlc_parser()
-    parser.feed_binary(0.0, b[0:5])
-    parser.feed_binary(0.5, b[5:7])
-    parser.feed_binary(1.0, b[7:])
-    for t, fr, fcs, crc_correct in parser:
-        print t, binascii.b2a_hex(fr), hex(fcs), crc_correct
+    if len(sys.argv) != 3:
+        print "Usage: hdlc_parser.py INPUT_LOG OUTPUT_LOG"
+        sys.exit(0)
+    log_name = sys.argv[1]
+    out_name = sys.argv[2]
+
+    in_parser = hdlc_parser()
+    out_parser = hdlc_parser()
+    with open(out_name, "w") as out:
+        metadata = None
+        for line in open(log_name):
+            if line.startswith('<') or line.startswith('>'):
+                ts, _, byt = line[1:].rstrip().split(None, 2)
+                ts = float(ts)
+                byt = int(byt)
+                metadata = [line[0], ts, byt]
+            elif line.startswith('#'):
+                out.write(line)
+            else:                
+                if not metadata:
+                    sys.stderr.write("Log Format Error 1.\n")
+                    sys.exit(1)
+
+                ts, byt = metadata[1:3]
+                b = line.rstrip().replace(" ", "")
+                b = binascii.a2b_hex(b)
+                # print metadata
+                if len(b) != byt:
+                    sys.stderr.write("Log Format Error 2.\n")
+                    sys.exit(1)
+                parser = (out_parser if metadata[0] == '<' else in_parser)
+                parser.feed_binary(ts, b)
+
+                for t, fr, fcs, crc_correct in parser:
+                    s1 = "OUT" if metadata[0] == '<' else "IN"
+                    s2 = "Succeed" if crc_correct else "Corrupted"
+                    out.write("%s%f %s %d\n" % (metadata[0], t, s1, len(fr)))
+                    out.write("#FCS:%s %s\n" % (hex(fcs), s2))
+                    out.write(" ".join(binascii.b2a_hex(c) for c in fr))
+                    out.write("\n")
+                metadata = None
