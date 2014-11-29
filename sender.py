eb113be2ca969a0c28770b61e9eb4e40057e4719
Samson <suchanoddnumber@gmail.com>
2014-11-28 16:50:34 -0800

    Send and receive messages from the phone
    You can send messages manually to the phone and receive the response
    through a terminal

---
+++ sender.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+sender.py
+COM port monitor. Work with SAMSUNG phone and com0com, a Null-modem emulator.
+To learn about com0com, see http://com0com.sourceforge.net/ .
+
+Author: Jiayao Li, Samson Richard Wong
+"""
+
+import sys
+import time
+import string
+import serial
+import serial.tools.list_ports
+import optparse
+import binascii
+
+
+def init_opt():
+    """
+    Initialize and return the option parser.
+    """
+    opt = optparse.OptionParser(prog="com-sender",
+                                usage="usage: %prog [options] LOGFILE ...",
+                                description="COM serial port sender.")
+    opt.add_option("-p", "--phy-serial-name",
+                    metavar="STR",
+                    action="store", type="string", dest="phy_serial_name", 
+                    help="Manually set the name of physical serial name.")
+    opt.add_option("--phy-baudrate",
+                    metavar="N",
+                    action="store", type="int", dest="phy_baudrate", 
+                    help="Set the physical baud rate [default: %default].")
+    opt.set_defaults(phy_baudrate=9600)
+    return opt
+
+
+def str_to_hex(s):
+    """
+    Convert each character of a string to its hex value, then return a
+    whitespace-separated string of hex values.
+    """
+    return " ".join(c.encode("hex") for c in s)
+
+def detect_ports():
+    """
+    Automatically detect and return the physical COM port name.
+    """
+    ports = serial.tools.list_ports.comports()
+    phy_ser_name = None
+    for name, description, hardware_id in ports:
+        # Find the physical name for SAMSUNG phone
+        # WARNING(Jiayao): in usual cases there will be more than one port found.
+        # On my computer, the lowest one is what we want. This may vary from 
+        # computer to computer.
+        if description.startswith("SAMSUNG Mobile USB Serial Port"):
+            if phy_ser_name is None or phy_ser_name > name:
+                phy_ser_name = name
+    return phy_ser_name
+
+
+if __name__ == "__main__":
+    opt = init_opt()
+    options, args = opt.parse_args(sys.argv[1:])
+
+    if options.phy_serial_name:
+        phy_ser_name = options.phy_serial_name
+    print "PHY COM: %s" % phy_ser_name
+
+    if phy_ser_name is None:
+        sys.stderr.write("Serial port name error.\n")
+        sys.exit(1)
+
+    phy_baudrate = options.phy_baudrate
+    print "PHY BAUD RATE: %d" % phy_baudrate
+
+    try:
+        # Open COM ports. A zero timeout means that IO functions never suspend.
+        phy_ser = serial.Serial(phy_ser_name, baudrate=phy_baudrate, timeout=.5)
+
+        while True:
+            s = raw_input('enter a command: ')
+            s = s.replace(" ", "")
+            s = binascii.a2b_hex(s)
+
+            if s:
+                phy_ser.write(s)
+
+            s = phy_ser.read(64)
+            if s:
+                print('reply: ' + str_to_hex(s))
+
+    except IOError, e:
+        sys.exit(e)
+    except Exception,e:
+        sys.exit(e)
