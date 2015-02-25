473b7f24f8f4fe8451004b6d5a4c46f99404e618
likayo <likayo25@gmail.com>
2015-02-24 17:50:13 -0800

    Supported stdin comm with tshark (partly)
    We still need to run text2pcap and wireshark each time; if we figure out
    how to send multiple packets to wireshark at the same time, we will
    improve efficiency.
    Text2pcap is buggy so we should replace it at some point.

--- automator/dm_log_packet/tshark.py
+++ automator/dm_log_packet/tshark.py
@@ -6,8 +6,7 @@ import subprocess
 
 
 class TShark:
-    WIRESHARK_PATH = "D:\\Program Files\\Wireshark\\"
-    TEMP_FILE = "temp.pcap"
+    TEMP_FILE = "temp.pcapng"
     FNULL = open(os.devnull, 'w')
     SUPPORTED_TYPES = ["LTE-RRC_PCCH", "LTE-RRC_DL_DCCH", "LTE-RRC_UL_DCCH"]
 
@@ -19,26 +18,37 @@ class TShark:
         """
         return msg_type.lower().replace("_", ".")
 
+
     @classmethod
     def decode_msg(cls, msg_type, b):
         if msg_type not in cls.SUPPORTED_TYPES:
-            return ""
-        input_data = "0 " + " ".join([binascii.b2a_hex(c) for c in b]) + " 00"
+            return "Unsupported message"
+        input_data = "000000 " + " ".join([binascii.b2a_hex(c) for c in b]) + " 00"
         # print input_data
         dissector_name = cls._dissector_name(msg_type)
-        proc = subprocess.Popen([cls.WIRESHARK_PATH + 'text2pcap',
-                                 '-l', '148', '-', cls.TEMP_FILE],
+        proc = subprocess.Popen(["text2pcap",
+                                 "-n",  # output pcapng format
+                                 "-l", "148",   # customized link layer type
+                                 "-",   # from stdin
+                                 # "-",   # to stdout
+                                 cls.TEMP_FILE,
+                                 ],
                                 stdin=subprocess.PIPE,
-                                stdout=cls.FNULL,
+                                stdout=subprocess.PIPE,
                                 stderr=cls.FNULL)
-        stdout_value, stderr_value = proc.communicate(input_data);
-        proc = subprocess.Popen([cls.WIRESHARK_PATH + 'tshark',
+        proc.stdin.write(input_data)
+        proc.stdin.close()
+        input_data = open(cls.TEMP_FILE, "rb").read()
+        # print repr(input_data)
+        proc2 = subprocess.Popen(['tshark',
                                  '-o', 'uat:user_dlts:"User 1 (DLT=148)","%s","0","","0",""' % dissector_name,
-                                 '-r', cls.TEMP_FILE,
+                                 '-r', "-",
                                  '-V',
                                  ],
-                                stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        stdout_value, stderr_value = proc.communicate(input_data);
+                                stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE,
+                                )
+        stdout_value, stderr_value = proc2.communicate(input_data);
         os.remove(cls.TEMP_FILE)
         return stdout_value
 
@@ -46,4 +56,4 @@ class TShark:
 if __name__ == '__main__':
     input_data = "4001BF281AEBA00000"
 
-    TShark.decode_msg("PCCH", binascii.a2b_hex(input_data))
+    print TShark.decode_msg("LTE-RRC_PCCH", binascii.a2b_hex(input_data))
