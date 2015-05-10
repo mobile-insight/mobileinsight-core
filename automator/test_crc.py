0cbb7c04d51956f89a5a9f6c1045394547c03b30
Likayo Lee <likayo25@gmail.com>
2015-05-09 19:24:32 -0700

    Remove unneccessary files.

--- automator/test_crc.py
+++
@@ -1,9 +0,0 @@
-import crcmod
-import crcmod.predefined
-from binascii import unhexlify
-
-s = unhexlify('5c0100c20100')
-
-crc16 = crcmod.predefined.Crc('x-25')
-crc16.update(s)
-print crc16.hexdigest() # => 0x2ba4
