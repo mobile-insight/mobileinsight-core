fc807eb13294e2e56d80135ecdc07d0ab062f3f7
Yuanjie Li <yuanjieli@Cs-210-143.CS.UCLA.EDU>
2015-05-13 08:22:40 -0700

    add msg_file.py

---
+++ automator/analyzer/msg_file.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+"""
+msg_file.py
+
+Dump the message to a file
+
+Author: Yuanjie Li
+"""
+
+from analyzer import *
+
+import xml.etree.ElementTree as ET
+import io
+
+class MsgFile(Analyzer):
+
+	def dump_message(self,msg):
+		self.file.write(ET.tostring(msg.data))
+
+
+
+	def __init__(self,filename):
+		Analyzer.__init__(self)
+
+		self.file=open(filename,'w')
+		self.add_source_callback(self.dump_message)
+
+	def __del__(self):
+		Analyzer.__del__(self)
+		self.file.close()
