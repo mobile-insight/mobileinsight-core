10ade69b967cc211f1b6708b3774c15c5223d1b6
Likayo Lee <likayo@ucla.edu>
2015-06-04 23:31:40 -0700

    Prototype C version of DMLogConfig

---
+++ dm_endec_c/consts.h
@@ -0,0 +1,11 @@
+#ifndef __DM_ENDEC_C_CONSTS_H__
+#define __DM_ENDEC_C_CONSTS_H__
+
+enum LOG_CONFIG_OP_ID {
+    DISABLE = 0,
+    GET_RANGE = 1,
+    SET_MASK = 3,
+    GET_MASK = 4
+};
+
+#endif	// __DM_ENDEC_C_CONSTS_H__
--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
-from distutils.core import setup
-import py2exe
+from distutils.core import setup, Extension
+try:
+    import py2exe
+    use_py2exe = True
+except ImportError:
+    use_py2exe = False
 
-# This is an temporary workaround to deal with importing errors using py2exe.
-if "py2exe" in sys.argv:
-    sys.path.append("./automator")
-    sys.path.append("./win_dep")
+if use_py2exe:
+    # This is an temporary workaround to deal with importing errors using py2exe.
+    if "py2exe" in sys.argv:
+        sys.path.append("./automator")
+        sys.path.append("./win_dep")
 
 PY2EXE_OPTIONS = {
     "bundle_files": 1,      # 1: bundle everything
 }
+
+c_dm_endec_module = Extension('automator.dm_endec.dm_endec_c',
+                                sources = ['dm_endec_c/dm_endec_c.cpp'],
+                                extra_compile_args=["-std=c++11"])
 
 setup(
     # setting for console program
@@ -21,7 +30,10 @@ setup(
             # "icon_resources": [(1, "resources/icon.ico")],
         },
     ],
+    packages = ['automator',
+                'automator.analyzer',
+                'automator.trace_collector',
+                'automator.dm_endec'],
     options = { 'py2exe' : PY2EXE_OPTIONS },
-    data_files = [("command_files", ["command_files/cmd_dict.txt",
-                                     "command_files/example_cmds.txt",])]
+    ext_modules = [c_dm_endec_module],
 )
