7aeaf61fea06783bea540e9c611c0b911eacf5c0
Yuanjie Li <yuanjie.li@cs.ucla.edu>
2015-06-23 14:55:45 +0800

    commit before checkout

--- mobile_insight/dm_endec/__init__.py
+++ mobile_insight/dm_endec/__init__.py
@@ -1,16 +1,9 @@
-"""
-dm_endec
-A module that encodes or decodes QCDM packets.
-
-Author: Jiayao Li
-"""
-
+# -*- coding: utf-8 -*-
 __all__ = [ "DMLogPacket",
             "DMLogConfigMsg",
             "WSDissector",
             "FormatError",
             "consts"]
-
 from dm_log_packet import DMLogPacket, FormatError
 from dm_log_config_msg import DMLogConfigMsg
 from ws_dissector import WSDissector
