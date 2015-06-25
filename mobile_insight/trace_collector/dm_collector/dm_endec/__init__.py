d6417ed3580a8402fd5da1c2d617d8e50cf57bf5
Yuanjie Li <yuanjie.li@cs.ucla.edu>
2015-06-25 11:05:17 +0800

    update

--- mobile_insight/trace_collector/dm_collector/dm_endec/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-__all__ = [ "DMLogPacket",
-            "WSDissector",
-            "FormatError",
-            "consts"]
-            "FormatError",]
-
-from dm_log_packet import DMLogPacket, FormatError
-from ws_dissector import WSDissector
