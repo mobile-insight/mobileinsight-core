8219cf7f67befbd0e1a01f924b71ce223f6799c3
Likayo Lee <likayo@ucla.edu>
2015-06-28 10:36:56 +0800

    Included ws_dissector in setup.py

--- setup.py
+++ setup.py
@@ -1,40 +1,13 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
-from distutils.core import setup, Extension
-try:
-    import py2exe
-    use_py2exe = True
-except ImportError:
-    use_py2exe = False
+from distutils.core import setup
 
-if use_py2exe:
-    # This is an temporary workaround to deal with importing errors using py2exe.
-    if "py2exe" in sys.argv:
-        sys.path.append("./mobile_insight")
-        sys.path.append("./win_dep")
-
-PY2EXE_OPTIONS = {
-    "bundle_files": 1,      # 1: bundle everything
-}
-
-
-
-dm_collector_c_module = Extension('mobile_insight.monitor.dm_collector.dm_collector_c',
-                                sources = [ "dm_collector_c/dm_collector_c.cpp",
-                                            "dm_collector_c/hdlc.cpp",
-                                            "dm_collector_c/log_config.cpp",
-                                            "dm_collector_c/log_packet.cpp",],
-                                extra_compile_args=["-std=c++11"])
+from distutils.command.install import INSTALL_SCHEMES
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
 
 setup(
-    # setting for console program
-    # console = [
-    #     {
-    #         "script": "mobile_insight/automator.py",
-    #         # "icon_resources": [(1, "resources/icon.ico")],
-    #     },
-    # ],
     name = 'MobileInsight',
     version = '1.0',
     description = 'Mobile network monitoring and analysis',
@@ -46,6 +19,5 @@ setup(
                 'mobile_insight.monitor.dm_collector',
                 'mobile_insight.monitor.dm_collector.dm_endec',
                 ],
-    options = { 'py2exe' : PY2EXE_OPTIONS },
-    ext_modules = [dm_collector_c_module],
+    data_files=[('ws_dissector', ['ws_dissector/ws_dissector'])],
 )
