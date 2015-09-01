9a6ee74baf0faa09f2d9fb558a0139291ce5879d
likayo <likayo25@gmail.com>
2015-09-01 11:53:06 +0800

    Reverted unexpected changes to ws_dissector to support Windows

--- ws_dissector/ws_dissector.cpp
+++ ws_dissector/ws_dissector.cpp
@@ -16,9 +16,20 @@
 
 #include "packet-aww.h"
 #include <stdio.h>
-#include <arpa/inet.h>
+#ifdef _WIN32
+    #include <winsock2.h>   // for ntohl()
+    #include <io.h>
+    #define SET_BINARY_MODE(handle) setmode(handle, O_BINARY)
+#elif __GNUC__
+    #include <arpa/inet.h>  // for ntohl()
+#else
+    #error Your compiler is not either MS Visual C compiler or GNU gcc.
+#endif
 
-#define WS_DISSECTOR_VERSION "1.0.1"
+typedef __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+
+#define WS_DISSECTOR_VERSION "1.0.2"
 
 const int BUFFER_SIZE = 2000;
 guchar buffer[BUFFER_SIZE] = {};
@@ -88,6 +99,16 @@ void try_dissect(epan_t *session, size_t data_len, const guchar* raw_data)
 
 int main(int argc, char** argv)
 {
+     // set stdin to binary mode
+#ifdef _WIN32
+     // On Windows, 0x1a will cause EOF in text mode, so we must change stdin to
+     // binary mode
+     SET_BINARY_MODE(fileno(stdin));
+#elif __GNUC__
+     // This step may not be necessary on Linux/OS X, so I commented it out
+     // freopen(NULL, "rb", stdin);
+#endif
+
     if (argc > 1) {
         printf("Version " WS_DISSECTOR_VERSION "\n");
         printf("Supported protocols:\n");
@@ -120,7 +141,7 @@ int main(int argc, char** argv)
     while (!feof(stdin)) {  // stop dissect when the pipe is closed
         fflush(stdin);
         fflush(stdout);
-        int ign = fread(buffer, sizeof(uint32_t), 2, stdin);
+        size_t ign = fread(buffer, sizeof(uint32_t), 2, stdin);
         if (ign < 2)
             break;
         unsigned int type = ntohl(*(uint32_t *)buffer);
