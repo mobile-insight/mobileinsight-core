b741916bbe08d75cb4123ad599d96d4e2d96b925
Likayo Lee <likayo@ucla.edu>
2015-09-28 15:42:07 -0700

    Updated to support distribution with python-for-android.

--- mobile_insight/monitor/dm_collector/__init__.py
+++ mobile_insight/monitor/dm_collector/__init__.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ["DMCollector", "FormatError", "DMLogPacket", "dm_collector_c"]
+__all__ = [
+            "DMCollector",  ### PFA: THIS LINE WILL BE DELETED ###
+            "FormatError",
+            "DMLogPacket",
+            "dm_collector_c"
+            ]
 
-from dm_collector import DMCollector
+from dm_collector import DMCollector    ### PFA: THIS LINE WILL BE DELETED ###
 from dm_endec import DMLogPacket, FormatError
 import dm_collector_c
