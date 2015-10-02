c1d8e3297adacc2d07bcb0a586b1a516bdc13813
Likayo Lee <likayo@ucla.edu>
2015-10-01 23:05:23 -0700

    Renamed PFA to P4A.

--- mobile_insight/monitor/dm_collector/__init__.py
+++ mobile_insight/monitor/dm_collector/__init__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 __all__ = [
-            "DMCollector",  ### PFA: THIS LINE WILL BE DELETED ###
+            "DMCollector",  ### P4A: THIS LINE WILL BE DELETED ###
             "FormatError",
             "DMLogPacket",
             "dm_collector_c"
             ]
 
-from dm_collector import DMCollector    ### PFA: THIS LINE WILL BE DELETED ###
+from dm_collector import DMCollector    ### P4A: THIS LINE WILL BE DELETED ###
 from dm_endec import DMLogPacket, FormatError
 import dm_collector_c
