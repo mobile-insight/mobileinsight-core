f1526ff18dbfc572ead652c91bae3a8cf800e179
Yuanjie Li <yuanjie.li@cs.ucla.edu>
2015-10-18 16:40:06 -0700

    a full version of profile abstraction

--- mobile_insight/analyzer/profile.py
+++ mobile_insight/analyzer/profile.py
@@ -8,27 +8,28 @@ Author: Yuanjie Li
 
 from jnius import autoclass
 
-class Node(object):
+import ast
+
+class ProfileNode(object):
     '''
-    A helper class for the ProfileHierarchy
+    A profile node for the ProfileHierarchy tree
     '''
     def __init__(self,name):
         self.name = name
-        self.children = [] # A node list to its children
-        self.__id = 0 # currently maximum index (useful for non-root nodes)
+        self.children = {} # A node list to its children
 
 
     def add(self,child):
         '''
-        Add a child 
+        Add a child node
 
         :param child: a child node
         :type child: string
 
         :returns: the added node
         '''
-        child_node = Node(child)
-        self.children.append(child_node)
+        child_node = ProfileNode(child)
+        self.children[child]=child_node
         return child_node
 
     def is_leaf(self):
@@ -46,6 +47,30 @@ class ProfileHierarchy(object):
 
     Given this hierarchy, the analyzer automatically builds underlying database, 
     and enforces query with hierarchical name (e.g., LTERrcAnalyzer.Reconfig.Drx.long_drx)
+
+    Example construction: consider the following RRC profile hierarchy
+
+        LteRrc
+          - Sib
+            - Sib1
+          - Reconfig
+            - MeasConfig
+            - Drx
+              - Short_drx
+              - Long_drx
+
+    The following code constructs such a profile hierachy:
+
+        LteRrcProfile = ProfileHierarchy('LteRrc')
+        root = LteRrcProfile.get_root();
+        sib=root.add('Sib');
+          sib.add('Sib1')
+          sib.add('Sib2')
+        reconfig=root.add('Reconfig');
+          measconfig=reconfig.add('MeasConfig')
+          drx=reconfig.add('Drx');
+            drx.add('Drx_short');
+            drx.add('Drx_long');
     '''
 
     def __init__(self,root):
@@ -55,7 +80,7 @@ class ProfileHierarchy(object):
         :param root: the root profile table name
         :type root: string
         '''
-        self.__root = Node(root)
+        self.__root = ProfileNode(root)
 
     def get_root(self):
         '''
@@ -90,7 +115,7 @@ class ProfileHierarchy(object):
 
             match = False
             # otherwise, update the node to the one that matches the new name
-            for child in cur_node.children:
+            for child in cur_node.children.values():
                 if child.name == nodes[count]:
                     cur_node = child
                     match = True
@@ -124,7 +149,7 @@ class Profile(object):
         self.__profile_hierarchy = profile_hierarchy
         self.__db = None
         self.__build_db()
-     
+ 
     def __create_table(self,node):
         '''
         Create SQL tables for the node
@@ -132,19 +157,11 @@ class Profile(object):
         :param node: a node in the profile hierarchy
         :type node: Node
         '''
-        if node.is_leaf():
-            return
+        sql_cmd = 'CREATE TABLE IF NOT EXISTS ' + node.name + "(id,profile,primary key(id))"
+        self.__db.execSQL(sql_cmd) 
 
-        sql_cmd = 'CREATE TABLE IF NOT EXISTS ' + node.name + "(id"
-        for child in node.children:
-            sql_cmd = sql_cmd + "," + child.name
-        
-        sql_cmd = sql_cmd + ")"
-        self.__db.execSQL(sql_cmd)
-
-        # Recursion
-        for child in node.children:
-            self.__create_table(child)    
+    def __get_root_name(self):
+        return self.__profile_hierarchy.get_root().name
 
     def __build_db(self):
         '''
@@ -160,85 +177,144 @@ class Profile(object):
 
             self.__create_table(root)
 
-    #TODO: finish query, insert and update methods for the profile
-    #query: given init id and hierarchy name, return the value
-    #insert/update: called by analyzers. Given id and name, insert a new item if not exist, or update it otherwise
-
-    def query(self,profile_name,init_id):
+    def query(self, init_id, profile_name):
         '''
         Query the profile value with a hierarchical name.
-        Example: self.query('LteRrc.Reconfig.Drx.Short_drx','cell_id=87')
+        Example: self.query('cell_id=87','LteRrc.Reconfig.Drx.Short_drx')
 
+        :id: the index in the root (e.g., Physical cell ID)
+        :type id: string
         :param profile_name: a hierarcical name separated by '.' (e.g., LteRrc.Reconfig.Drx.Short_drx)
         :type profile_name: string
-        :init_id: the index in the root of the hierarchical name (e.g., Physical cell ID)
-        :type init_id: string
-        :returns: value list that satisfies the query, or None if the profile does not exist
+        :returns: value list that satisfies the query, or None if no such field (id not exist, incomplete record, etc.)
         '''
 
-        #Step 1: check if the profile_name is valid
+        #Step 1: check if the name conforms to the hierarchy
         if self.__profile_hierarchy is None: #no profile defined
             return None
-        node = self.__profile_hierarchy.get_node(profile_name)
-        if node is None or not node.is_leaf():
+        #Check if the field to query is valid
+        profile_node = self.__profile_hierarchy.get_node(profile_name)
+        if profile_node is None:
             return None
 
-        nodes = profile_name.split('.')
-
-        #Step 2: generate the SQL statement with JOIN operator
-        #In above example, the SQL command is
-        #    select Drx.Short_drx from LteRrc,Reconfig,Drx
-        #    where LteRrc.id=87 and LteRrc.Reconfig=Reconfig.id and Reconfig.Drx=Drx.id
-
-        sql_cmd = "select "+ nodes[-2]+"."+nodes[-1]
-        tmp = profile_name.replace('.',',')
-        tmp_index = tmp.rfind(',')
-        sql_cmd = sql_cmd + " from "+tmp[0:tmp_index]
-        sql_cmd = sql_cmd + " where "+nodes[0]+".id="+init_id
-        for i in range(0,len(nodes)-2):
-            sql_cmd = sql_cmd + " and "+nodes[i]+"."+nodes[i+1]+"="+nodes[i+1]+".id"
-
-      
-        #Step 3: if the return value is empty, return None, otherwise the first item
+        #Step 2: extract the raw profile
+        sql_cmd = "select profile from "+self.__get_root_name()+" where id="+init_id
         sql_res = self.__db.rawQuery(sql_cmd,None)
         sql_res.moveToFirst();
-        res = []
-        while True:
-            res.append(sql_res.getString(0))
-            if not sql_res.moveToNext():
+        res = ast.literal_eval(sql_res.getString(0)) #convert string to dictionary
+        if not res: #the id does not exist
+            return None
+
+        #Step 3: extract the result from raw profile
+        profile_nodes = profile_name.split('.')
+        for i in range(1,len(profile_nodes)):
+            if res is None: #no profile
                 break
+            res = res[profile_nodes[i]]   
         return res
 
-    def insert(self,profile_name,init_id,value):
-        '''
-        Insert/update a profile value 
-        Example: self.insert('LteRrc.Reconfig.Drx',{‘Short_Drx':'1s','Long_Drx':'5s'}) 
 
-        :param profile_name: a hierarcical name separated by '.' (e.g., LteRrc.Reconfig.Drx.Short_drx)
-        :type profile_name: string
-        :init_id: the index in the root of the hierarchical name (e.g., Physical cell ID)
+        
+
+    def update(self, init_id, profile_name, value_dict):
+        
+        '''
+        Update a profile value
+        Example: self.update('87','LteRrc.Reconfig.Drx',{Drx_short:1,Drx_long:5})
+
+        If the init_id does not exist, create a new item in the root, with specified values and all other fields as "null" 
+
+        Otherwise, update the specified field values, and keep the ramaining values unchanged. 
+
+        The update operation is atomic. No partial update would be performed
+
+        :param init_id: the initial id in the root
         :type init_id: string
-        :value: a dictionary of value_name:value
-        :type value: dictionary
+        :param profile_name: a hierarcical name separated by '.' (e.g., LteRrc.Reconfig.Drx)
+        :type profile_name: string
+        :param value: a field_name->value dictionary of the specified updated values. 
+        All the field names should appear in the profile_name.
+        :type value: string->string dictionary
+        :returns: True if the update succeeds, False otherwise
         '''
 
-        #Step 1: check if the profile_name is valid
+        #Step 1: check if the name conforms to the hierarchy
         if self.__profile_hierarchy is None: #no profile defined
-            return None
-        node = self.__profile_hierarchy.get_node(profile_name)
-        if node is None:
-            return None
-        #The profile node should have all its children as leaves 
-        valid_node=True
-        for child in node.children:
-            if not child.is_leaf():
-                valid_node = False
-                break
-        if not valid_node:
-            return None	
+            return False
+        #Check if the field to update is valid
+        test_node = self.__profile_hierarchy.get_node(profile_name)
+        if test_node is None:
+            return False
+        #Check the value fileds to update are indeed included based on hierarchy
+        for field_name in value_dict:
+            if field_name not in test_node.children:
+                #Invalid node
+                return False
 
-        #Step 2: Update EVERY database in a reverse order
-        nodes = profile_name.split('.')
+        #Step 2: check if the id exists or not
+        sql_cmd = "select profile from "+self.__get_root_name()+" where id="+init_id
+        sql_res = self.__db.rawQuery(sql_cmd,None)
+        
+        profile_nodes = profile_name.split('.')
+        # if not query_res: 
+        if sql_res.getColumnCount()==0:
+            #The id does not exist. Create a new record
 
+            query_res = {}
+            res=query_res
+            profile_node = self.__profile_hierarchy.get_root()
+
+
+            for i in range(1,len(profile_nodes)-1):
+                #Initialization
+                for child in profile_node.children:
+                    res[child]=None
+                res[profile_nodes[i]]={}
+                res=res[profile_nodes[i]]
+                profile_node = profile_node.children[profile_nodes[i]]
+
+            res[profile_nodes[len(profile_nodes)-1]]=value_dict
+
+            #Insert the new record into table
+            insert_values = autoclass("android.content.ContentValues")
+            insert_values.put("id",init_id)
+            insert_values.put("profile","\""+str(query_res)+"\"")
+            self.__db.insert(self.__get_root_name(),null,insert_values)
+        else:
+            sql_res.moveToFirst();
+            query_res = ast.literal_eval(sql_res.getString(0)) #convert string to dictionary
+            #The id exists. Update the record
+            res=query_res
+            profile_node = self.__profile_hierarchy.get_root()
+            for i in range(1,len(profile_nodes)-1):
+                if res[profile_nodes[i]] is not None:
+                    res = res[profile_nodes[i]]
+                else:
+                    res[profile_nodes[i]] = {}
+                    res = res[profile_nodes[i]]
+                    for child in profile_node.children:
+                        res[child]=None
+            for item in value_dict:
+                res[item]=value_dict[item]
+            update_values = autoclass("android.content.ContentValues")
+            update_values.put("profile","\""+str(query_res)+"\"")
+            self.__db.update(self.__get_root_name(),update_values,"id="+init_id,None)
+
+if __name__=="__main__":
+
+    #Create a profile
+    profile_hierarchy=ProfileHierarchy('LteRrc');
+    root=profile_hierarchy.get_root()
+    sib=root.add('Sib');
+    reconfig=root.add('Reconfig');
+    drx=reconfig.add('Drx');
+    drx.add('Drx_short');
+    drx.add('Drx_long');
+
+    profile=Profile(profile_hierarchy)
+
+    res = profile.update("87",'LteRrc.Reconfig.Drx',{'Drx_short':'1','Drx_long':'5'})
+    
+    print profile.query("87",'LteRrc.Reconfig.Drx')
 
 
