fb2275fda1d3e16452107b0bab3a1f25ea513a76
Likayo Lee <likayo@ucla.edu>
2015-10-23 18:36:51 -0700

    Added AndroidQmdlMonitor

--- mobile_insight/monitor/__init__.py
+++ mobile_insight/monitor/__init__.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
 __all__ = [
+            "AndroidQmdlMonitor"
             "Monitor",
             "DMCollector",  ### P4A: THIS LINE WILL BE DELETED ###
             "Replayer",
             "QmdlReplayer",
             ]
 
+from android_qmdl_monitor import AndroidQmdlMonitor
 from monitor import Monitor
 from dm_collector import *
 from replayer import Replayer
