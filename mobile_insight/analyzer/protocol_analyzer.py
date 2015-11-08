4a8bd6b1924d6ecb6462c76e3c0c1bad8f9eea20
Yuanjie Li <yuanjie.li@cs.ucla.edu>
2015-11-08 11:09:05 -0800

    New abstraction: ProtocolAnalyzer

--- mobile_insight/analyzer/protocol_analyzer.py
+++ mobile_insight/analyzer/protocol_analyzer.py
@@ -39,4 +39,39 @@ class ProtocolAnalyzer(Analyzer):
         """
 
         Analyzer.__init__(self)
-        
+        self.__profile = Profile(self.__create_profile_hierarchy())
+        self.__state_machine = StateMachnie(self.__create_state_machine())
+
+        #Update state dynamics
+        self.add_source_callback(self.__update_state)
+      
+
+    def __create_profile_hierarchy():
+        """
+        Declare a protocol-specific ProfileHierarchy
+
+        returns: ProfileHierarchy based on protocol-specific parameters
+        """
+        return None
+
+    def __create_state_machine():
+        """
+        Declare a state machine
+
+        returns: ProfileHierarchy based on protocol-specific FSMs
+        """
+        return None
+
+    def declare_failure_flag():
+        """
+        Initialize failure flags
+        """
+        pass
+
+    def __update_state(self,msg):
+        """
+        Update states in response to cellular events
+
+        :param msg: the event (message) from the trace collector.
+        """
+        self.__state_machine.update_state(msg)
