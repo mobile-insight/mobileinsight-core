58f2f20a3443df66fed9059d239d3352b534a4c7
Yuanjie Li <yuanjie.li@cs.ucla.edu>
2016-07-18 11:38:22 -0700

    update LtePhyAnalyzer (for DASH demo)
    Former-commit-id: 26d49154fb5b270e05cd2f43fe8bcd5c43219780

--- mobile_insight/analyzer/lte_phy_analyzer.py
+++ mobile_insight/analyzer/lte_phy_analyzer.py
@@ -55,19 +55,36 @@ class LtePhyAnalyzer(Analyzer):
         if msg.type_id=="LTE_PHY_PDSCH_Packet":
             log_item = msg.data.decode()
 
-            # self.log_info(str(hex(log_item["PDSCH RNTIl ID"])))
-
             if not self.init_timestamp:
                 self.init_timestamp = log_item['timestamp']
                 self.log_info("0 "
                 + str(log_item["MCS 0"])+" "
                 + str(log_item["MCS 1"])+" "
                 + str(log_item["TBS 0"])+" "
-                + str(log_item["TBS 1"]))
+                + str(log_item["TBS 1"])+" "
+                + str(log_item["PDSCH RNTI Type"]))
             else:
                 self.log_info(str((log_item['timestamp']-self.init_timestamp).total_seconds())+" "
                 + str(log_item["MCS 0"])+" "
                 + str(log_item["MCS 1"])+" "
                 + str(log_item["TBS 0"])+" "
-                + str(log_item["TBS 1"])) 
+                + str(log_item["TBS 1"])+" "
+                + str(log_item["PDSCH RNTI Type"])) 
+
+
+            # if not self.init_timestamp:
+            #     self.init_timestamp = log_item['timestamp']
+            #     self.log_info("0s "
+            #     + "MCS0=" + str(log_item["MCS 0"])+" "
+            #     + "MCS1=" + str(log_item["MCS 1"])+" "
+            #     + "TBS0=" + str(log_item["TBS 0"])+" "
+            #     + "TBS1=" + str(log_item["TBS 1"])+" "
+            #     + "C-RNTI=" + str(log_item["PDSCH RNTI Type"]))
+            # else:
+            #     self.log_info(str((log_item['timestamp']-self.init_timestamp).total_seconds())+"s "
+            #     + "MCS0=" + str(log_item["MCS 0"])+" "
+            #     + "MCS1=" + str(log_item["MCS 1"])+" "
+            #     + "TBS0=" + str(log_item["TBS 0"])+" "
+            #     + "TBS1=" + str(log_item["TBS 1"])+" "
+            #     + "C-RNTI=" + str(log_item["PDSCH RNTI Type"])) 
         	
