b370ee4cd9fba93de7187d537b4521cd7fc3f3ca
Yuanjie Li <yuanjie.li@cs.ucla.edu>
2016-07-27 22:07:36 -0700

    MsgLogger: save decoded message as text file
    Former-commit-id: 649cf48e6f694363c188a67c71e28ef05fe3fec7

--- examples/offline-analysis-example.py
+++ examples/offline-analysis-example.py
@@ -9,13 +9,19 @@ Offline analysis by replaying logs
 
 #Import MobileInsight modules
 from mobile_insight.monitor import OfflineReplayer
-from mobile_insight.analyzer import LteRrcAnalyzer,WcdmaRrcAnalyzer,LteNasAnalyzer,UmtsNasAnalyzer, LtePhyAnalyzer, LteMacAnalyzer
+from mobile_insight.analyzer import MsgLogger, LteRrcAnalyzer,WcdmaRrcAnalyzer,LteNasAnalyzer,UmtsNasAnalyzer, LtePhyAnalyzer, LteMacAnalyzer
 
 if __name__ == "__main__":
     
     # Initialize a 3G/4G monitor
     src = OfflineReplayer()
     src.set_input_path("./offline_log_example.mi2log")
+
+    logger = MsgLogger()
+    logger.set_decode_format(MsgLogger.XML)
+    logger.set_dump_type(MsgLogger.FILE_ONLY)
+    logger.save_decoded_msg_as("./test.txt")
+    logger.set_source(src)
     
     #Analyzers
     lte_rrc_analyzer = LteRrcAnalyzer()
