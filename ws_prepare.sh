c2aa8b616e1e2cfbdec00616b6df5acbf26d5c0f
Zengwen Yuan <zyuan@cs.ucla.edu>
2016-11-23 11:31:19 -0800

    add migration script for macOS, seperate Wireshark lib dependency from MobileInsight
    Former-commit-id: 42c939b8d3f9bbaa9bf01285568d93712af79a1b
    Former-commit-id: 380b54de75c29978ea0551a82bc080ddb73353e9

---
+++ ws_prepare.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# Migration script for mobileInsight desktop version on macOS
+# It deletes old libraries installed and use standard
+# installation location /usr/local/lib
+# Author  : Zengwen Yuan
+# Date    : 2016-11-22
+# Version : 1.0
+
+# Use your local library path
+LD_LIBRARY_PATH=/usr/local/lib
+# This is where this MobileInsight source is extracted
+MOBILE_INSIGHT_SRC_PATH=${HOME}/MobileInsight-2.1.0
+# A copy of Wireshark sources will be put inside the MobileInsight source folder
+WIRESHARK_SRC_PATH=${MOBILE_INSIGHT_SRC_PATH}/wireshark-2.0.8
+
+# Clean up libraries installed by the old version of MobileInsight
+sudo rm /usr/lib/libglib-2.0.dylib
+sudo rm /usr/lib/libgobject-2.0.dylib
+sudo rm /usr/lib/libgio-2.0.dylib
+sudo rm /usr/lib/libgmodule-2.0.dylib
+sudo rm /usr/lib/libgthread-2.0.dylib
+sudo rm /usr/lib/libwireshark.6.dylib
+sudo rm /usr/lib/libwireshark.dylib
+sudo rm /usr/lib/libwiretap.5.dylib
+sudo rm /usr/lib/libwiretap.dylib
+sudo rm /usr/lib/libwsutil.6.dylib
+sudo rm /usr/lib/libwsutil.dylib
+
+# Install up-to-date Homebrew and use default prefixes
+# You can check related environment variables using `brew config`
+# HOMEBREW_PREFIX: /usr/local
+# HOMEBREW_REPOSITORY: /usr/local/Homebrew
+# HOMEBREW_CELLAR: /usr/local/Cellar
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# First let Homebrew install Wireshark 2.2.2 once, and solve dependencies
+brew install wireshark
+# Make sure compile environment and other dependencies are installed
+brew install pkg-config cmake glib gettext libffi
+# Remove Wireshark 2.2.2 and install the old stable version 2.0.8 using our own formulae
+brew remove wireshark
+brew install ${MOBILE_INSIGHT_SRC_PATH}/wireshark.rb
+brew link --overwrite wireshark
+
+# Download necessary source files to compile ws_dissector
+# Make sure the package version is in accordance to the version that was installed by Homebrew
+if [ ! -d "${WIRESHARK_SRC_PATH}" ]; then
+	wget https://1.na.dl.wireshark.org/src/wireshark-2.0.8.tar.bz2
+	tar -xjvf wireshark-2.0.8.tar.bz2
+	rm wireshark-2.0.8.tar.bz2
+fi
+
+# Generate config.h for Wireshark 2.0.8
+cd ${WIRESHARK_SRC_PATH}
+./configure --disable-wireshark
+
+# Compile ws_dissector
+cd ${MOBILE_INSIGHT_SRC_PATH}/ws_dissector
+if [ -e "ws_dissector" ]; then
+	rm ws_dissector
+fi
+g++ ws_dissector.cpp packet-aww.cpp -o ws_dissector `pkg-config --libs --cflags glib-2.0` \
+	-I"${WIRESHARK_SRC_PATH}" -L"${LD_LIBRARY_PATH}" -lwireshark -lwsutil -lwiretap
+strip ws_dissector
+
+# Install compiled MobileInsight desktop version
+cd ${MOBILE_INSIGHT_SRC_PATH}
+sudo python setup.py install
+rm -r ${MOBILE_INSIGHT_SRC_PATH}/libs
+
+# Run example
+echo "\\n\\n"
+echo "Successfully installed the newest version of MobileInsight desktop version!"
+echo "Testing the offline analysis example."
+cd ${MOBILE_INSIGHT_SRC_PATH}/examples
+python ./offline-analysis-example.py
