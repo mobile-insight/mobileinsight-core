11b53674b2c3d4516e09745b8fbfcd17fac4be5b
Yuanjie Li <yuanjie.li@cs.ucla.edu>
2017-05-04 16:48:31 -0700

    bug fix
    Former-commit-id: 7aaed19e3841a86cd0cad5af6a49021370fbe375
    Former-commit-id: 9cc5949c2c440ba1e9fa0c6964d8ae812c4e7f14

--- mobile_insight/monitor/__init__.py
+++ mobile_insight/monitor/__init__.py
@@ -25,4 +25,5 @@ from monitor import Monitor
 from dm_collector import * ### P4A: THIS LINE WILL BE DELETED ###
 from offline_replayer import OfflineReplayer
 from online_monitor import OnlineMonitor
+from android_muxraw_monitor import AndroidMuxrawMonitor
 # from muxraw_parser import *
