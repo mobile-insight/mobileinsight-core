5238dca65f53ddce03df856a14271300073e9f42
Zhehui Zhang <happyqiaomai@163.com>
2017-07-28 14:14:04 +0000

    update: add API LteRlcAnalyzer.RLC_(UL/DL)_RB_(SETTING/NUMBER)

--- mobile_insight/analyzer/lte_rlc_analyzer.py
+++ mobile_insight/analyzer/lte_rlc_analyzer.py
@@ -41,12 +41,41 @@ class LteRlcAnalyzer(Analyzer):
 
         if msg.type_id == "LTE_RLC_UL_Config_Log_Packet" or msg.type_id == "LTE_RLC_DL_Config_Log_Packet":
             log_item = msg.data.decode()
+            # print log_item
             subPkt = log_item['Subpackets'][0]
             if 'Released RBs' in subPkt:
                 for releasedRBItem in subPkt['Released RBs']:
                     rbConfigIdx = releasedRBItem['Released RB Cfg Index']
                     if rbConfigIdx in self.rbInfo:
                         self.rbInfo.pop(rbConfigIdx)
+            rb_num = 0
+            # print subPkt
+            for subpacket in subPkt['Active RBs']:
+                # print subpacket
+                rb_num += 1
+                lc_id = subpacket['LC ID']
+                ack_mode = subpacket['RB Mode']
+                rb_type = subpacket['RB Type']
+                bcast_dict = {}
+                bcast_dict['lcid'] = lc_id
+                bcast_dict['ack mode'] = ack_mode
+                bcast_dict['rb type'] = rb_type
+                bcast_dict['timstamp'] = str(log_item['timestamp'])
+                if msg.type_id == "LTE_RLC_UL_Config_Log_Packet":
+                    self.broadcast_info('RLC_UL_RB_SETTING', bcast_dict)
+                    self.log_info('RLC_UL_RB_SETTING: ' + str(bcast_dict))
+                else:
+                    self.broadcast_info('RLC_DL_RB_SETTING', bcast_dict)
+                    self.log_info('RLC_DL_RB_SETTING: ' + str(bcast_dict))
+            bcast_dict = {}
+            bcast_dict['number'] = str(rb_num)
+            bcast_dict['timstamp'] = str(log_item['timestamp'])
+            if msg.type_id == "LTE_RLC_UL_Config_Log_Packet":
+                self.broadcast_info('RLC_UL_RB_NUMBER', bcast_dict)
+                self.log_info('RLC_UL_RB_NUMBER: ' + str(bcast_dict))
+            else:
+                self.broadcast_info('RLC_DL_RB_NUMBER', bcast_dict)
+                self.log_info('RLC_DL_RB_NUMBER: ' + str(bcast_dict))
 
         if msg.type_id == "LTE_RLC_UL_AM_All_PDU":
             log_item = msg.data.decode()
