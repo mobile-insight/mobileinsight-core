23817a8f0d062b43063840da2f6092ab60109175
Zhuo <juejiang91@gmail.com>
2019-03-28 13:37:13 -0400

    add pusch_tx_report 24 pucch_csf 24

--- dm_collector_c/lte_phy_pucch_csf.h
+++ dm_collector_c/lte_phy_pucch_csf.h
@@ -10,6 +10,27 @@ const Fmt LtePhyPucchCsf_Fmt [] = {
     {UINT, "Version", 1},
 };
 
+const Fmt LtePhyPucchCsf_Payload_v24 [] = {
+    {UINT, "Start System Sub-frame Number", 2}, // 4 bits
+    {PLACEHOLDER, "Start System Frame Number", 0},  // 10 bits
+    {UINT, "PUCCH Reporting Mode", 1},  // last 2 bits in previous byte + 1 bit
+    {PLACEHOLDER, "PUCCH Report Type", 0},    // 4 bit
+    {PLACEHOLDER, "Size BWP",0},
+    {UINT, "Number of Subbands", 4},    // 4 bits
+    {PLACEHOLDER, "BWP Index",0},
+    {PLACEHOLDER, "Rank Index", 0}, // skip 3 bits, 1 bit
+    {PLACEHOLDER, "SubBand Label",0},
+    {PLACEHOLDER, "CQI CW0", 0},   // skip 2 bits, 4 bits
+    {PLACEHOLDER, "CQI CW1", 0},   // 4 bits
+    {PLACEHOLDER, "Wideband PMI", 0},   // 4 bits
+    {PLACEHOLDER, "Carrier Index", 0},  // 4 bits
+    {PLACEHOLDER, "CSF Tx Mode", 0},    // 4 bits
+    {UINT, "Num Csirs Ports", 1},
+    {PLACEHOLDER, "Wideband PMI1", 0},
+    {UINT,"Pti",1},
+    {SKIP, NULL, 2},
+};
+
 const Fmt LtePhyPucchCsf_Payload_v43 [] = {
     {UINT, "Start System Sub-frame Number", 2}, // 4 bits
     {PLACEHOLDER, "Start System Frame Number", 0},  // 10 bits
@@ -52,6 +73,115 @@ static int _decode_lte_phy_pucch_csf_payload (const char *b,
     int temp;
 
     switch (pkt_ver) {
+    case 24:
+        {
+            offset += _decode_by_fmt(LtePhyPucchCsf_Payload_v24,
+                    ARRAY_SIZE(LtePhyPucchCsf_Payload_v24, Fmt),
+                    b, offset, length, result);
+            temp = _search_result_int(result, "Start System Sub-frame Number");
+            int iSubFN = temp & 15;
+            int iSysFN = (temp >> 4) & 1023;
+            int ileft = (temp >> 14) & 3;
+            temp = _search_result_int(result, "PUCCH Reporting Mode");
+            int iPucchReportingMode = ((temp & 1) << 2) | ileft;
+            int iPucchReportType = (temp >> 1) & 15;
+            int iSizeBWP =(temp>>5) & 7;
+
+            unsigned int utemp = _search_result_uint(result, "Number of Subbands");
+            int iNumSubbands = utemp & 15;
+            int iBWPIndex = (utemp >>4) & 7;
+            int iRankIndex = (utemp >> 7) & 1;
+            int iSubBandLabel = (utemp >>8) & 3;
+            int iCQI0 = (utemp >> 10) & 15;
+            int iCQI1 = (utemp >> 14) & 15;
+            int iWidebandPMI = (utemp >> 18) & 15;
+            int iCarrierIndex = (utemp >> 22) & 15;
+            int iCSFTxMode = (utemp >> 26) & 15;
+
+            utemp =_search_result_uint(result, "Num Csirs Ports");
+            int iNumCsirsPorts=utemp & 0xf;
+            int iWidebandPMI1 = (utemp>>4) & 0xf;
+
+            utemp =_search_result_uint(result, "Pti");
+            int iPti = utemp & 0x1;
+
+            old_object = _replace_result_int(result,
+                    "Start System Sub-frame Number", iSubFN);
+            Py_DECREF(old_object);
+            old_object = _replace_result_int(result,
+                    "Start System Frame Number", iSysFN);
+            Py_DECREF(old_object);
+            old_object = _replace_result_int(result, "PUCCH Reporting Mode",
+                    iPucchReportingMode);
+            Py_DECREF(old_object);
+            (void) _map_result_field_to_name(result, "PUCCH Reporting Mode",
+                    ValueNamePucchReportingMode,
+                    ARRAY_SIZE(ValueNamePucchReportingMode, ValueName),
+                    "(MI)Unknown");
+            old_object = _replace_result_int(result, "PUCCH Report Type",
+                    iPucchReportType);
+            Py_DECREF(old_object);
+            (void) _map_result_field_to_name(result, "PUCCH Report Type",
+                    ValueNamePucchReportType,
+                    ARRAY_SIZE(ValueNamePucchReportType, ValueName),
+                    "(MI)Unknown");
+            old_object = _replace_result_int(result, "Size BWP",
+                    iSizeBWP);
+            Py_DECREF(old_object);
+            old_object = _replace_result_int(result, "Rank Index",
+                    iRankIndex);
+            Py_DECREF(old_object);
+            (void) _map_result_field_to_name(result, "Rank Index",
+                    ValueNameRankIndex,
+                    ARRAY_SIZE(ValueNameRankIndex, ValueName),
+                    "(MI)Unknown");
+            old_object = _replace_result_int(result, "Number of Subbands",
+                    iNumSubbands);
+            Py_DECREF(old_object);
+            old_object = _replace_result_int(result, "BWP Index",
+                    iBWPIndex);
+            Py_DECREF(old_object);
+            old_object = _replace_result_int(result, "SubBand Label",
+                    iSubBandLabel);
+            Py_DECREF(old_object);
+            old_object = _replace_result_int(result, "CQI CW0",
+                    iCQI0);
+            Py_DECREF(old_object);
+            old_object = _replace_result_int(result, "CQI CW1",
+                    iCQI1);
+            Py_DECREF(old_object);
+            old_object = _replace_result_int(result, "Wideband PMI",
+                    iWidebandPMI);
+            Py_DECREF(old_object);
+            old_object = _replace_result_int(result, "CSF Tx Mode",
+                    iCSFTxMode);
+            Py_DECREF(old_object);
+            (void) _map_result_field_to_name(result, "CSF Tx Mode",
+                    ValueNameCSFTxMode,
+                    ARRAY_SIZE(ValueNameCSFTxMode, ValueName),
+                    "(MI)Unknown");
+            old_object = _replace_result_int(result, "Carrier Index",
+                    iCarrierIndex);
+            Py_DECREF(old_object);
+            (void) _map_result_field_to_name(result, "Carrier Index",
+                    ValueNameCarrierIndex,
+                    ARRAY_SIZE(ValueNameCarrierIndex, ValueName),
+                    "(MI)Unknown");
+
+            old_object = _replace_result_int(result, "Num Csirs Ports",
+                    iNumCsirsPorts);
+            Py_DECREF(old_object);
+
+            old_object = _replace_result_int(result, "Wideband PMI1",
+                    iWidebandPMI1);
+            Py_DECREF(old_object);
+
+            old_object = _replace_result_int(result, "Pti",
+                    iPti);
+            Py_DECREF(old_object);
+
+            return offset - start;
+        }
     case 43:
         {
             offset += _decode_by_fmt(LtePhyPucchCsf_Payload_v43,
--- dm_collector_c/lte_phy_pusch_tx_report.h
+++ dm_collector_c/lte_phy_pusch_tx_report.h
@@ -10,6 +10,59 @@ const Fmt LtePhyPuschTxReport_Fmt [] = {
     {UINT, "Version", 1},
 };
 
+const Fmt LtePhyPuschTxReport_Payload_v24 [] = {
+    {UINT, "Serving Cell ID", 2},    // 9 bits
+    {PLACEHOLDER, "Number of Records", 0},  // 5 bits
+    {SKIP, NULL, 1},
+    {UINT, "Dispatch SFN SF", 2},
+    {SKIP, NULL, 2},
+};
+
+const Fmt LtePhyPuschTxReport_Record_v24 [] = {
+//modify byte length and remove some fields Num repetition/RB NB Start Index
+    {UINT, "Current SFN SF", 2},
+    {UINT, "Coding Rate Data", 2},  // x/1024.0
+    {UINT, "ACK", 4},   // 1 bit
+    {PLACEHOLDER, "CQI", 0},    // 1 bit
+    {PLACEHOLDER, "RI", 0}, // 1 bit
+    {PLACEHOLDER, "Frequency Hopping", 0},  // 2 bits
+    {PLACEHOLDER, "Redund Ver", 0}, // 2 bits
+    {PLACEHOLDER, "Mirror Hopping", 0}, // 2 bits
+    {PLACEHOLDER, "Cyclic Shift of DMRS Symbols Slot 0 (Samples)", 0},  // 4 bits
+    {PLACEHOLDER, "Cyclic Shift of DMRS Symbols Slot 1 (Samples)", 0},  // 4 bits
+    {PLACEHOLDER, "DMRS Root Slot 0", 0},   // 11 bits
+    {PLACEHOLDER, "UE SRS", 0}, // 1 bit
+    {UINT, "DMRS Root Slot 1", 4},  // 11 bits
+    {PLACEHOLDER, "Start RB Slot 0", 0},    // 7 bits
+    {PLACEHOLDER, "Start RB Slot 1", 0},    // 7 bits
+    {PLACEHOLDER, "Num of RB", 0},  // 7 bits
+    {UINT, "PUSCH TB Size", 2},
+
+    {UINT, "Num ACK Bits", 2},
+    {PLACEHOLDER, "ACK Payload", 0},   // 20 bits, bytestream
+
+    {UINT, "Rate Matched ACK Bits", 4},
+    {PLACEHOLDER, "Num RI Bits NRI (bits)", 0}, // right shift 1 bit, 3 bits
+    {PLACEHOLDER, "RI Payload", 0},    // 4 bits
+    {PLACEHOLDER, "Rate Matched RI Bits", 0},   // 11 bits
+    {PLACEHOLDER, "PUSCH Mod Order", 0},    // 2 bits
+    {PLACEHOLDER, "RI Payload2", 0},    // 4 bits
+
+
+    {UINT, "PUSCH Digital Gain (dB)", 1},    // 8 bits
+
+    {UINT, "SRS Occasion", 1},   // 1 bit
+    {PLACEHOLDER, "Re-tx Index", 0},    // 3 bits
+    {SKIP, NULL, 2},
+
+    {UINT, "PUSCH Tx Power (dBm)", 4},    // 10 bits
+    {PLACEHOLDER, "Num CQI Bits", 0},   // 8 bits
+    {PLACEHOLDER, "Rate Matched CQI Bits", 0},  // 14 bits
+
+    {BYTE_STREAM, "CQI Payload", 16},
+    {UINT, "Tx Resampler", 4},
+};
+
 const Fmt LtePhyPuschTxReport_Payload_v26 [] = {
     {UINT, "Serving Cell ID", 2},    // 9 bits
     {PLACEHOLDER, "Number of Records", 0},  // 5 bits
@@ -291,6 +344,200 @@ static int _decode_lte_phy_pusch_tx_report_payload (const char *b,
     PyObject *pystr;
 
     switch (pkt_ver) {
+    case 24:
+        {
+            offset += _decode_by_fmt(LtePhyPuschTxReport_Payload_v24,
+                    ARRAY_SIZE(LtePhyPuschTxReport_Payload_v24, Fmt),
+                    b, offset, length, result);
+            temp = _search_result_int(result, "Serving Cell ID");
+            int iServingCellId = temp & 511;    // 9 bits
+            int num_record = (temp >> 9) & 31;  // 5 bits
+            old_object = _replace_result_int(result, "Number of Records",
+                    num_record);
+            Py_DECREF(old_object);
+            old_object = _replace_result_int(result, "Serving Cell ID",
+                    iServingCellId);
+            Py_DECREF(old_object);
+
+            PyObject *result_record = PyList_New(0);
+            for (int i = 0; i < num_record; i++) {
+                PyObject *result_record_item = PyList_New(0);
+                offset += _decode_by_fmt(LtePhyPuschTxReport_Record_v24,
+                        ARRAY_SIZE(LtePhyPuschTxReport_Record_v24, Fmt),
+                        b, offset, length, result_record_item);
+                temp = _search_result_int(result_record_item, "Coding Rate Data");
+                float fCodingRateData = temp / 1024.0;
+                pyfloat = Py_BuildValue("f", fCodingRateData);
+                old_object = _replace_result(result_record_item,
+                        "Coding Rate Data", pyfloat);
+                Py_DECREF(old_object);
+                Py_DECREF(pyfloat);
+
+                u_temp = _search_result_uint(result_record_item, "ACK");
+                int iAck = u_temp & 1;    // 1 bit
+                int iCQI = (u_temp >> 1) & 1;   // 1 bit
+                int iRI = (u_temp >> 2) & 1;    // 1 bit
+                int iFrequencyHopping = (u_temp >> 3) & 3;  // 2 bits
+                int iRedundVer = (u_temp >> 5) & 3; // 2 bits
+                int iMirrorHopping = (u_temp >> 7) & 3; // 2 bits
+                int iCSDSS0 = (u_temp >> 9) & 15;   // 4 bits
+                int iCSDSS1 = (u_temp >> 13) & 15;  // 4 bits
+                int iDMRSRootSlot0 = (u_temp >> 17) & 2047; // 11 bits
+                int iUESRS = (u_temp >> 28) & 1;    // 1 bit
+                old_object = _replace_result_int(result_record_item, "ACK",
+                        iAck);
+                Py_DECREF(old_object);
+                (void) _map_result_field_to_name(result_record_item,
+                        "ACK", ValueNameExistsOrNone,
+                        ARRAY_SIZE(ValueNameExistsOrNone, ValueName),
+                        "(MI)Unknown");
+                old_object = _replace_result_int(result_record_item, "CQI",
+                        iCQI);
+                Py_DECREF(old_object);
+                (void) _map_result_field_to_name(result_record_item,
+                        "CQI", ValueNameExistsOrNone,
+                        ARRAY_SIZE(ValueNameExistsOrNone, ValueName),
+                        "(MI)Unknown");
+                old_object = _replace_result_int(result_record_item, "RI",
+                        iRI);
+                Py_DECREF(old_object);
+                (void) _map_result_field_to_name(result_record_item,
+                        "RI", ValueNameExistsOrNone,
+                        ARRAY_SIZE(ValueNameExistsOrNone, ValueName),
+                        "(MI)Unknown");
+                old_object = _replace_result_int(result_record_item,
+                        "Frequency Hopping", iFrequencyHopping);
+                Py_DECREF(old_object);
+                (void) _map_result_field_to_name(result_record_item,
+                        "Frequency Hopping", ValueNameEnableOrDisable,
+                        ARRAY_SIZE(ValueNameEnableOrDisable, ValueName),
+                        "(MI)Unknown");
+                old_object = _replace_result_int(result_record_item,
+                        "Redund Ver", iRedundVer);
+                Py_DECREF(old_object);
+                old_object = _replace_result_int(result_record_item,
+                        "Mirror Hopping", iMirrorHopping);
+                Py_DECREF(old_object);
+                old_object = _replace_result_int(result_record_item,
+                        "Cyclic Shift of DMRS Symbols Slot 0 (Samples)",
+                        iCSDSS0);
+                Py_DECREF(old_object);
+                old_object = _replace_result_int(result_record_item,
+                        "Cyclic Shift of DMRS Symbols Slot 1 (Samples)",
+                        iCSDSS1);
+                Py_DECREF(old_object);
+                old_object = _replace_result_int(result_record_item,
+                        "DMRS Root Slot 0", iDMRSRootSlot0);
+                Py_DECREF(old_object);
+                old_object = _replace_result_int(result_record_item,
+                        "UE SRS", iUESRS);
+                Py_DECREF(old_object);
+                (void)_map_result_field_to_name(result_record_item,
+                        "UE SRS",
+                        ValueNameOnOrOff,
+                        ARRAY_SIZE(ValueNameOnOrOff, ValueName),
+                        "(MI)Unknown");
+
+                u_temp = _search_result_uint(result_record_item, "DMRS Root Slot 1");
+                int iDMRSRootSlot1 = u_temp & 2047; // 11 bits
+                int iStartRBSlot0 = (u_temp >> 11) & 127;   // 7 bits
+                int iStartRBSlot1 = (u_temp >> 18) & 127;   // 7 bits
+                int iNumOfRB = (u_temp >> 25) & 127;    // 7 bits
+                old_object = _replace_result_int(result_record_item,
+                        "DMRS Root Slot 1", iDMRSRootSlot1);
+                Py_DECREF(old_object);
+                old_object = _replace_result_int(result_record_item,
+                        "Start RB Slot 0", iStartRBSlot0);
+                Py_DECREF(old_object);
+                old_object = _replace_result_int(result_record_item,
+                        "Start RB Slot 1", iStartRBSlot1);
+                Py_DECREF(old_object);
+                old_object = _replace_result_int(result_record_item,
+                        "Num of RB", iNumOfRB);
+                Py_DECREF(old_object);
+
+                u_temp = _search_result_uint(result_record_item, "Num ACK Bits");
+                int iNumACKBits = u_temp & 0x7;// 3 bit
+                int iACKPayload = (u_temp>>3) & 0x15;// 4 bit
+                old_object = _replace_result_int(result_record_item,
+                        "Num ACK Bits", iNumACKBits);
+                Py_DECREF(old_object);
+                old_object = _replace_result_int(result_record_item,
+                        "ACK Payload", iACKPayload);
+                Py_DECREF(old_object);
+
+                u_temp = _search_result_uint(result_record_item, "Rate Matched ACK Bits");
+                int iRateMatchedACKBits = u_temp & 0x7ff;//11 bit
+                int iNumRIBit = (u_temp>>11) & 0x3;//2 bit
+                int iRIPayload = (u_temp>>13) & 0x3;//2 bit
+                int iRateMatchedRIBits=(u_temp >> 15) & 0x7ff;//11 bit
+                int iPUSCHModOrder = (u_temp >> 26) & 0x3;//2 bit
+                int iRIPayload2 =(u_temp >>28) & 0x15;//4 bit
+
+                old_object = _replace_result_int(result_record_item,
+                        "Rate Matched ACK Bits", iRateMatchedACKBits);
+                Py_DECREF(old_object);
+                old_object = _replace_result_int(result_record_item,
+                        "Num RI Bits NRI (bits)", iNumRIBit);
+                Py_DECREF(old_object);
+                old_object = _replace_result_int(result_record_item,
+                        "RI Payload", iRIPayload);
+                Py_DECREF(old_object);
+                old_object = _replace_result_int(result_record_item,
+                        "Rate Matched RI Bits", iRateMatchedRIBits);
+                Py_DECREF(old_object);
+                old_object = _replace_result_int(result_record_item,
+                        "PUSCH Mod Order", iPUSCHModOrder);
+                Py_DECREF(old_object);
+
+                (void)_map_result_field_to_name(result_record_item,
+                        "PUSCH Mod Order",
+                        ValueNameModulation,
+                        ARRAY_SIZE(ValueNameModulation, ValueName),
+                        "(MI)Unknown");
+
+                old_object = _replace_result_int(result_record_item,
+                        "RI Payload2", iRIPayload2);
+                Py_DECREF(old_object);
+
+                u_temp = _search_result_uint(result_record_item, "SRS Occasion");
+                int iSRSOccasion = u_temp & 0x1;// 1 bit
+                int iRetxIndex = (u_temp >>1) & 0x1f;//5 bit
+                old_object = _replace_result_int(result_record_item,
+                        "SRS Occasion", iSRSOccasion);
+                Py_DECREF(old_object);
+                old_object = _replace_result_int(result_record_item,
+                        "Re-tx Index", iRetxIndex);
+                Py_DECREF(old_object);
+
+                u_temp = _search_result_uint(result_record_item, "PUSCH Tx Power (dBm)");
+                int iPUSCHTxPower = u_temp & 0x3ff;// 10 bit
+                int iNumCqiBits = (u_temp >>10) & 0xff;//8 bit
+                int iRateMatchedCqiBits =(u_temp>>18) & 0x3fff;//14 bit
+
+                old_object = _replace_result_int(result_record_item,
+                        "PUSCH Tx Power (dBm)", iPUSCHTxPower);
+                Py_DECREF(old_object);
+                old_object = _replace_result_int(result_record_item,
+                        "Num CQI Bits", iNumCqiBits);
+                Py_DECREF(old_object);
+                old_object = _replace_result_int(result_record_item,
+                        "Rate Matched CQI Bits", iRateMatchedCqiBits);
+                Py_DECREF(old_object);
+
+                PyObject *t1 = Py_BuildValue("(sOs)", "Ignored",
+                        result_record_item, "dict");
+                PyList_Append(result_record, t1);
+                Py_DECREF(t1);
+                Py_DECREF(result_record_item);
+            }
+            PyObject *t = Py_BuildValue("(sOs)", "Records",
+                    result_record, "list");
+            PyList_Append(result, t);
+            Py_DECREF(t);
+            Py_DECREF(result_record);
+            return offset - start;
+        }
     case 26:
         {
             offset += _decode_by_fmt(LtePhyPuschTxReport_Payload_v26,
