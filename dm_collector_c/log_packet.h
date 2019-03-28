dfe6b70ec39f4022e5cbf36e90ce30a0cfefa8fa
Zhuo <juejiang91@gmail.com>
2019-03-28 16:11:08 -0400

    update PDSCH_Demapper 104(pixel-xl)

--- dm_collector_c/log_packet.h
+++ dm_collector_c/log_packet.h
@@ -541,6 +541,7 @@ const Fmt LtePhyPdschDemapperConfigFmt_v104 [] = {
     {UINT, "Frequency Selective PMI", 1},   // right shift 1 bit, 2 bits
     {PLACEHOLDER, "PMI Index", 0},  // 4 bits
     {UINT, "Transmission Scheme", 1},   // 4 bits
+    {SKIP,NULL,2},
     {UINT, "TBS 0", 2},
     {UINT, "MCS 0", 2},
     {PLACEHOLDER, "Traffic to Pilot Ratio", 0},
