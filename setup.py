8f69aac055495f7dfd1275fc61110cf790267067
luckiday <guoyunqi@gmail.com>
2019-11-02 16:37:41 -0700

    Setup update for python 3

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-#!/bin/python
 # MobileInsight package installation script
 
 import urllib.request, urllib.parse, urllib.error
@@ -13,6 +12,7 @@ from setuptools import setup, Extension
 
 try:
     import py2exe
+
     use_py2exe = True
 except ImportError:
     use_py2exe = False
@@ -24,59 +24,64 @@ if use_py2exe:
         sys.path.append("./win_dep")
 
 PY2EXE_OPTIONS = {
-    "bundle_files": 1, # bundle everything
+    "bundle_files": 1,  # bundle everything
 }
 
 # =============================================================================
 # Remove the "-Wstrict-prototypes" compiler option, which isn't valid for C++.
 import distutils.sysconfig
+
 cfg_vars = distutils.sysconfig.get_config_vars()
 for key, value in list(cfg_vars.items()):
-        if type(value) == str:
-                    cfg_vars[key] = value.replace("-Wstrict-prototypes", "")
+    if type(value) == str:
+        cfg_vars[key] = value.replace("-Wstrict-prototypes", "")
 # =============================================================================
 
 dm_collector_c_module = Extension('mobile_insight.monitor.dm_collector.dm_collector_c',
-                                sources = [ "dm_collector_c/dm_collector_c.cpp",
-                                            "dm_collector_c/export_manager.cpp",
-                                            "dm_collector_c/hdlc.cpp",
-                                            "dm_collector_c/log_config.cpp",
-                                            "dm_collector_c/log_packet.cpp",
-                                            "dm_collector_c/utils.cpp",],
-                                define_macros=[ ('EXPOSE_INTERNAL_LOGS', 1), ]
-                                )
+                                  sources=["dm_collector_c/dm_collector_c.cpp",
+                                           "dm_collector_c/export_manager.cpp",
+                                           "dm_collector_c/hdlc.cpp",
+                                           "dm_collector_c/log_config.cpp",
+                                           "dm_collector_c/log_packet.cpp",
+                                           "dm_collector_c/utils.cpp", ],
+                                  define_macros=[('EXPOSE_INTERNAL_LOGS', 1), ]
+                                  )
 
-def parse_libs(url,suffix):
-    pattern  = '<a href=".*%s.*">.*</a>' % suffix
+
+def parse_libs(url, suffix):
+    pattern = '<a href=".*%s.*">.*</a>' % suffix
     response = urllib.request.urlopen(url).read()
-    res      = []
+    res = []
     for item in re.findall(pattern, response):
         lib_start = item.find('href="')
-        lib_end   = item.find('">')
-        lib_name  = item[lib_start + 6:lib_end]
+        lib_end = item.find('">')
+        lib_name = item[lib_start + 6:lib_end]
         res.append(lib_name)
     return res
 
-def download_libs(url, libs, chmod = True):
+
+def download_libs(url, libs, chmod=True):
     for lib in libs:
         if not os.path.isfile("./libs/" + lib):
-            urllib.request.urlretrieve (url + lib, "./libs/" + lib)
+            urllib.request.urlretrieve(url + lib, "./libs/" + lib)
     if chmod:
         for lib in libs:
             os.chmod("./libs/" + lib, 0o755 | stat.S_IEXEC)
 
+
 def download_win(url):
     if not os.path.isfile("./ws_dissector/ws_dissector.exe"):
-            urllib.request.urlretrieve (url+"ws_dissector.exe", "./ws_dissector/ws_dissector.exe")
+        urllib.request.urlretrieve(url + "ws_dissector.exe", "./ws_dissector/ws_dissector.exe")
     if not os.path.isfile("./mobile_insight/monitor/dm_collector/dm_collector_c.pyd"):
-        urllib.request.urlretrieve (url+"dm_collector_c.pyd", "./mobile_insight/monitor/dm_collector/dm_collector_c.pyd")
+        urllib.request.urlretrieve(url + "dm_collector_c.pyd",
+                                   "./mobile_insight/monitor/dm_collector/dm_collector_c.pyd")
 
 
 print("Building libraries...")
 
 if platform.system() == "Darwin" or platform.system() == "Linux":
     PACKAGE_DATA = {'mobile_insight.monitor.dm_collector': ['./dm_collector_c.so']}
-    DATA_FILES   = []
+    DATA_FILES = []
 
 elif platform.system() == "Windows":
     # Download necessary libraries
@@ -94,39 +99,39 @@ elif platform.system() == "Windows":
         print(("Unsupported operating system: " + str(arch)))
         sys.exit()
 
-    libs = parse_libs(url,"dll")
+    libs = parse_libs(url, "dll")
     download_libs(url, libs, False)
     download_win(url)
 
     PACKAGE_DATA = {'mobile_insight.monitor.dm_collector': ['./dm_collector_c.pyd']}
-    ws_files     = ['ws_dissector/ws_dissector.exe']
+    ws_files = ['ws_dissector/ws_dissector.exe']
     # include all dlls
     for root, dirs, files in os.walk('ws_dissector'):
         ws_files.extend(['ws_dissector\\' + name for name in files if name.endswith('.dll')])
 
-    lib_list     = ["./libs/" + x for x in os.listdir("./libs/")]
-    DATA_FILES   = [(sys.exec_prefix + '\\mobile_insight\\ws_dissector\\', ws_files),
+    lib_list = ["./libs/" + x for x in os.listdir("./libs/")]
+    DATA_FILES = [(sys.exec_prefix + '\\mobile_insight\\ws_dissector\\', ws_files),
                   (sys.exec_prefix + '\\mobile_insight\\ws_dissector\\', lib_list)]
 else:
-    print(("Unsupported operating system: " + str(arch)))
+    print(("Unsupported operating system: " + str(platform.system())))
     sys.exit()
 
 setup(
-    name         = 'MobileInsight',
-    version      = '4.0.0',
-    description  = 'Mobile network monitoring and analysis',
-    author       = 'UCLA Wireless Networking Group and Purdue Peng Group',
-    author_email = 'support@mobileinsight.net',
-    url          = 'http://www.mobileinsight.net',
-    license      = 'Apache License 2.0',
-    packages     = ['mobile_insight',
-                    'mobile_insight.analyzer',
-                    'mobile_insight.monitor',
-                    'mobile_insight.monitor.dm_collector',
-                    'mobile_insight.monitor.dm_collector.dm_endec',
-                ],
-    package_data = PACKAGE_DATA,
-    data_files   = DATA_FILES,
-    options      = { 'py2exe' : PY2EXE_OPTIONS },
-    ext_modules  = [dm_collector_c_module],
+    name='MobileInsight',
+    version='4.0.0',
+    description='Mobile network monitoring and analysis',
+    author='UCLA Wireless Networking Group and Purdue Peng Group',
+    author_email='support@mobileinsight.net',
+    url='http://www.mobileinsight.net',
+    license='Apache License 2.0',
+    packages=['mobile_insight',
+              'mobile_insight.analyzer',
+              'mobile_insight.monitor',
+              'mobile_insight.monitor.dm_collector',
+              'mobile_insight.monitor.dm_collector.dm_endec',
+              ],
+    package_data=PACKAGE_DATA,
+    data_files=DATA_FILES,
+    options={'py2exe': PY2EXE_OPTIONS},
+    ext_modules=[dm_collector_c_module],
 )
