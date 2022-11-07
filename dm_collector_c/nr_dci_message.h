f88b385c3df20c12111c43c35b42395f00a9d0f1
Matthew Martin <matt.martin7717@gmail.com>
2022-11-06 19:21:18 -0800

    changed to big endian formatting

--- dm_collector_c/nr_dci_message.h
+++ dm_collector_c/nr_dci_message.h
@@ -8,33 +8,33 @@
 
 // 4 bytes
 const Fmt NrDciMessage_MacVersion_Fmt[] = {
-        {UINT, "Minor Version", 2}, // 16 bits
-        {UINT, "Major Version", 2}, // 16 bits
+        {UINT_BIG_ENDIAN, "Minor Version", 2}, // 16 bits
+        {UINT_BIG_ENDIAN, "Major Version", 2}, // 16 bits
         {PLACEHOLDER, "Major.Minor Version", 0}, // 0 bits
         {PLACEHOLDER, "{id: 2026230 }", 0} // 0 bits
 }
 
 // 12 bytes
 const Fmt NrDciMessage_Version_Fmt[] = {
-        {UINT, "Sleep", 1}, // 8 bits
-        {UINT, "Beam Change", 1}, // 8 bits
-        {UINT, "Signal Change", 1}, // 8 bits
-        {UINT, "DL Dynamic Cfg Change", 1}, // 8 bits
-        {UINT, "DL Config", 1}, // 8 bits
-        {UINT, "UL Config", 1}, // 4 bits, 1 byte = 8 bits cover UL Config and ML1 State Change, do bit masking on the 1 byte to get UL Config
+        {UINT_BIG_ENDIAN, "Sleep", 1}, // 8 bits
+        {UINT_BIG_ENDIAN, "Beam Change", 1}, // 8 bits
+        {UINT_BIG_ENDIAN, "Signal Change", 1}, // 8 bits
+        {UINT_BIG_ENDIAN, "DL Dynamic Cfg Change", 1}, // 8 bits
+        {UINT_BIG_ENDIAN, "DL Config", 1}, // 8 bits
+        {UINT_BIG_ENDIAN, "UL Config", 1}, // 4 bits, 1 byte = 8 bits cover UL Config and ML1 State Change, do bit masking on the 1 byte to get UL Config
         {PLACEHOLDER, "ML1 State Change", 0}, // 4 bits, do bit masking on the 1 byte to get ML1 State Change
         {SKIP, NULL, 2}, // 16 bits for Reserved
-        {UINT, "Log Fields Change BMask",  2} // 16 bits
+        {UINT_BIG_ENDIAN, "Log Fields Change BMask",  2} // 16 bits
         {SKIP, NULL, 1}, // 8 bits for Reserved
-        {UINT, "Num Records", 1} // 8 bits
+        {UINT_BIG_ENDIAN, "Num Records", 1} // 8 bits
 }
 
 // 8 bytes
 const Fmt NrDciMessage_Record_Fmt[] = {
-        {UINT, "Slot", 1}, // 8 bits
-        {UINT, "Num", 1}, // 8 bits, 1 byte = 8 bits cover Num and Reserved0 (which we don't need), do bit masking on the 8 bits to get Num
-        {UINT, "Frame" 2}, // 8 bits (1 byte) + last 2?? bits of the 2nd byte. 2 bytes cover Frame and Reserved (which we don't need). Do bit masking on the 2 bytes to get the Frame.
-        {UINT, "Num DCI", 1}, // 8 bits
+        {UINT_BIG_ENDIAN, "Slot", 1}, // 8 bits
+        {UINT_BIG_ENDIAN, "Num", 1}, // 8 bits, 1 byte = 8 bits cover Num and Reserved0 (which we don't need), do bit masking on the 8 bits to get Num
+        {UINT_BIG_ENDIAN, "Frame" 2}, // 8 bits (1 byte) + last 2?? bits of the 2nd byte. 2 bytes cover Frame and Reserved (which we don't need). Do bit masking on the 2 bytes to get the Frame.
+        {UINT_BIG_ENDIAN, "Num DCI", 1}, // 8 bits
         {SKIP, NULL, 3} // 24 bits for reserved[0], reserved[1], reserved[2], which we don't need
 }
 
@@ -44,7 +44,7 @@ const ValueName ValueNameNrDciMessage_Num[] = {
 
 // 4 bytes
 const Fmt NrDciMessage_DciInfo_Fmt[] = {
-        {UINT, "Carrier ID", 4}, // 2?? bits, 4 bytes = 32 bits cover Carrier ID, RNTI Type, DCI Format, Aggregation Level, Raw DCI Included, and Reserve (not needed). do bit masking to get Carrier ID
+        {UINT_BIG_ENDIAN, "Carrier ID", 4}, // 2?? bits, 4 bytes = 32 bits cover Carrier ID, RNTI Type, DCI Format, Aggregation Level, Raw DCI Included, and Reserve (not needed). do bit masking to get Carrier ID
         {PLACEHOLDER, "RNTI Type", 0}, // 6?? bits, do bit masking on the 4 bytes to get RNTI Type
         {PLACEHOLDER, "DCI Format", 0}, // 8?? bits, do bit masking on the 4 bytes to get DCI Format
         {PLACEHOLDER, "Aggregation Level", 0}, // 8 bits, do bit masking on the 4 bytes to get Aggregation Level
@@ -53,14 +53,14 @@ const Fmt NrDciMessage_DciInfo_Fmt[] = {
 
 // 12 bytes
 const Fmt NrDciMessage_RawDci_Fmt[] = {
-        {UINT, "Raw DCI[0]", 4}, // 32 bits
-        {UINT, "Raw DCI[1]", 4}, // 32 bits
-        {UINT, "Raw DCI[2]", 4} // 32 bits
+        {UINT_BIG_ENDIAN, "Raw DCI[0]", 4}, // 32 bits
+        {UINT_BIG_ENDIAN, "Raw DCI[1]", 4}, // 32 bits
+        {UINT_BIG_ENDIAN, "Raw DCI[2]", 4} // 32 bits
 }
 
 // 16 bytes
 const Fmt NrDciMessage_UL_0_1_Fmt[] = {
-        {UINT, "DCI Format", 4}, // 2?? bits, 4 bytes = 32 bits cover DCI Format, Carrier ID, NDI, MCS, Freq Hopping Flag, RV, HARQ ID, PUSCH TPC, UL SUL Ind, PTRS DMRS Association, Beta Offset Ind. Do bit masking on the 2 bytes to get DCI Format
+        {UINT_BIG_ENDIAN, "DCI Format", 4}, // 2?? bits, 4 bytes = 32 bits cover DCI Format, Carrier ID, NDI, MCS, Freq Hopping Flag, RV, HARQ ID, PUSCH TPC, UL SUL Ind, PTRS DMRS Association, Beta Offset Ind. Do bit masking on the 2 bytes to get DCI Format
         {PLACEHOLDER, "Carrier ID", 0}, // 4?? bits, do bit masking on the 4 bytes to get Carrier ID
         {PLACEHOLDER, "NDI", 0}, // 2?? bits, do bit masking on the 4 bytes to get NDI
         {PLACEHOLDER, "MCS", 0}, // ?? bits, do bit masking on the 4 bytes to get MCS
@@ -73,21 +73,21 @@ const Fmt NrDciMessage_UL_0_1_Fmt[] = {
         {PLACEHOLDER, "BWP Ind", 0}, // ?? bits, do bit masking on the 4 bytes to get BWP Ind
         {PLACEHOLDER, "PTRS DMRS Association", 0}, // ?? bits, do bit masking on the 4 bytes to get PTRS DMRS Association
         {PLACEHOLDER, "Beta Offset Ind", 0}, // ?? bits, do bit masking on the 4 bytes to get Beta Offset Ind
-        {UINT, "RB Assignment", 4}, // ?? bits, 4 bytes = 32 bits cover RB Assignment, UL SCH Ind, DAI 1, DAI 2, SRS Resource Indication, Precoding Layer Info. Do bit masking on the 4 bytes to get RB Assignment
+        {UINT_BIG_ENDIAN, "RB Assignment", 4}, // ?? bits, 4 bytes = 32 bits cover RB Assignment, UL SCH Ind, DAI 1, DAI 2, SRS Resource Indication, Precoding Layer Info. Do bit masking on the 4 bytes to get RB Assignment
         {PLACEHOLDER, "UL SCH Ind", 0}, // ?? bits, do bit masking on the 4 bytes to get UL SCH Ind
         {PLACEHOLDER, "DAI 1", 0}, // ?? bits, do bit masking on the 4 bytes to get DAI 1
         {PLACEHOLDER, "DAI 2", 0}, // ?? bits, do bit masking on the 4 bytes to get DAI 2
         {PLACEHOLDER, "SRS Resource Indication", 0}, // ?? bits, do bit masking on the 4 bytes to get SRS Resource Indication
         {PLACEHOLDER, "Precoding Layer Info", 0}, // ?? bits, do bit masking on the 4 bytes to get Precoding Layer Info
-        {UINT, "Antenna Ports", 2}, // ? bits, 2 byte = 16 bits cover Antenna Ports, DMRS Seq Init Flag, SRS Request, CSI Request, CBGTI, Reserved MCE Enable, RA Type. Do bit masking on the 2 bytes to get Antenna Ports
+        {UINT_BIG_ENDIAN, "Antenna Ports", 2}, // ? bits, 2 byte = 16 bits cover Antenna Ports, DMRS Seq Init Flag, SRS Request, CSI Request, CBGTI, Reserved MCE Enable, RA Type. Do bit masking on the 2 bytes to get Antenna Ports
         {PLACEHOLDER, "DMRS Seq Init Flag", 0}, // ? bits, do bit masking on the 2 bytes to get DMRS Seq Init Flag
         {PLACEHOLDER, "SRS Request", 0}, // ? bits, do bit masking on the 2 bytes to get SRS Request
         {PLACEHOLDER, "CSI Request", 0}, // ? bits, do bit masking on the 2 bytes to get CSI Request
         {PLACEHOLDER, "CBGTI", 0}, // ? bits, do bit masking on the 2 bytes to get CBGTI
         {PLACEHOLDER, "Reserved MCE Enable", 0}, // ? bits, do bit masking on the 2 bytes to get Reserved MCE Enable
         {PLACEHOLDER, "RA Type", 0}, // ? bits, do bit masking on the 2 bytes to get RA Type
-        {UINT, "Prune Reason", 1}, // ? bits, 1 byte = 8 bits covers Prune Reason MSB and Reserved (not needed). Do bit masking on the 1 byte to get Prune Reason
-        {UINT, "Pruned Mask", 4} // 32 bits
+        {UINT_BIG_ENDIAN, "Prune Reason", 1}, // ? bits, 1 byte = 8 bits covers Prune Reason MSB and Reserved (not needed). Do bit masking on the 1 byte to get Prune Reason
+        {UINT_BIG_ENDIAN, "Pruned Mask", 4} // 32 bits
 }
 
 // todo - don't know how many bytes each field is (no video)
@@ -152,26 +152,6 @@ static int _decode_nr_DCI (const char *b,
         utemp = _search_result_uint(record, "Num");
         unsigned int num = utemp & 0xF; // last 4 bits is num
         old_object = _replace_result_int(record, "Num", num);
-        Py_DECREF(old_object);
-
-        /*
-        28 02 = 0010 1000 0000 0010 - note this is in little endian!
-        Frame = 552 = 28 x2  = 0010 1000 xxxx 0010
-        Reserved = 0 = xx 02 = xxxx xxxx 0000 0010
-
-        552 in hexadecimal is 0x228 = 1000 1010 0000
-
-        So x = (Frame & 0xFF03) = 0x2802 - to get 552, we need 0x0228
-        We get the leftmost 4 bits and shift it 8 bits to the left: y = (x & 0x000F) << 8 = 0x0200
-        We shift x right 8 bits: z = x >> 8 = 0x0028
-        Now we do ans = y | z = 0x0200 | 0x0028 = 0x0228 = 552!
-        */
-        utemp = _search_result_uint(record, "Frame");
-        unsigned int x = (utemp & 0xFF03);
-        unsigned int y = (x & 0x000F) << 8;
-        unsigned int z = x >> 8;
-        unsigned int frame = y | z;
-        old_object = _replace_result_int(record, "Frame", frame);
         Py_DECREF(old_object);
 
         // for each record, build a list of DCIs (there may be > 1 DCIs in one record, so we need to loop through each individual DCI to build the list)
